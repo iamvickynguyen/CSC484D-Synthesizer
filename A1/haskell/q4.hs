3d7a4751c9d68278b342722dfddd0949ff047ed6
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-02-01 14:19:24 -0800

    what happened with System.Random?

--- A1/haskell/q4.hs
+++ A1/haskell/q4.hs
@@ -1,5 +1,6 @@
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Builder as B
+import System.Random
 import Data.Foldable
 
 sawtooth :: [Float] -> [Float]
@@ -17,15 +18,24 @@ pulse :: [Float] -> [Float]
 pulse samples =
  map (\x -> if (ceiling(x) `mod` 10) < 5 then 1.0 else 0.0) samples
 
+noise :: [Float] -> [Float]
+noise samples =
+ take (length samples) randomRIO(0,1::Float) 
+
 main :: IO()
 main = do
  let srate = 44100
  let duration = 3
  let x = [0, 1.0/srate..(duration)]
+ print (length x)
  let sawtoothWave = sawtooth x 
  B.writeFile "sawtooth.bin" $ B.toLazyByteString $ fold $ map B.floatLE sawtoothWave
+ print (length x)
  let triangleWave = triangle x
  B.writeFile "triangle.bin" $ B.toLazyByteString $ fold $ map B.floatLE triangleWave
+ print (length x)
  let pulseWave = pulse x
  B.writeFile "pulse.bin" $ B.toLazyByteString $ fold $ map B.floatLE pulseWave
- 
+ print (length x)
+ let noiseSound = noise x
+ B.writeFile "noise.bin" $ B.toLazyByteString $ fold $ map B.floatLE noiseSound
