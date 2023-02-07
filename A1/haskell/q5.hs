c0597a5d99ba3e1af6f4b13a904ded04374a34d0
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-02-06 17:40:04 -0800

    done Q9-Q5-Q1

--- A1/haskell/q5.hs
+++ A1/haskell/q5.hs
@@ -2,13 +2,20 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Builder as B
 import Data.Foldable
 
-data WavetableOscillator = WavetableOscillator
-{
- phaseIndex :: Float,
- tableLength :: Int,
- waveTable :: a,
- nSamples :: Int,
- points :: [Float],
- freq :: Int,
- srate :: Int
-}
+createWavetable :: Float -> (Float -> Float) -> [Float]
+createWavetable len fn =
+ map (\x -> fn (2 * pi * x)) t
+ where
+  t = [0,1.0/(len + 1)..1]
+
+process :: [Float] -> Int -> [Float]
+process wavetable n =
+ (concat $ take i $ repeat wavetable) ++ (take remain wavetable)
+ where
+  (i, remain) = n `divMod` n
+
+main :: IO()
+main = do
+ let sound = createWavetable 44100.0 sin
+ let output = process sound (44100 * 3)
+ B.writeFile "out.bin" $ B.toLazyByteString $ fold $ map B.floatLE output
